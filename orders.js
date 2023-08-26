e78918cc85f98054a23b12076b566e8cf6ddda25
ijeh ekene <97108614+Ijeh06@users.noreply.github.com>
2023-08-25 19:54:39 -0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Bespoke-Suits',
+        productNumber: '85743',
+        paymentStatus: 'Due',
+        status: 'pending'
+    },
+    {
+        productName: 'Wedding Gown',
+        productNumber: '97245',
+        paymentStatus: 'Refunded',
+        status:'Declined'
+    },
+    {
+        productName: 'Bubu-gown',
+        productNumber: '36452',
+        paymentStatus: 'Paid',
+        status:'Active' 
+    },
+]
